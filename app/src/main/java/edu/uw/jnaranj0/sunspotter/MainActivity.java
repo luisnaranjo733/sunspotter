d0f6783ff2f874bb8ca062e5528e8f62fc9618b7
Luis Naranjo <luisnaranjo733@gmail.com>
2016-01-15 18:13:08 -0800

    button + getting text is working

--- app/src/main/java/edu/uw/jnaranj0/sunspotter/MainActivity.java
+++ app/src/main/java/edu/uw/jnaranj0/sunspotter/MainActivity.java
@@ -2,12 +2,23 @@ package edu.uw.jnaranj0.sunspotter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Sunspotter";
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+    }
+    public void onBtnClicked(View view) {
+        if (view.getId() == R.id.button) {
+            Log.v(TAG, "Button clicked!" + editText.getText());
+        }
     }
 }
